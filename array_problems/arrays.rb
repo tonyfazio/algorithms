61e99e57649cf625b7e8bf2bd1646885e3c24e4d
Tony Fazio <antoniojfazio@gmail.com>
2014-04-25 14:19:10 -0500

    Finished max for real

--- array_problems/arrays.rb
+++ array_problems/arrays.rb
@@ -3,6 +3,24 @@ require 'pry-debugger'
 
 module ArrayUtil
   def self.max(array)
+    p=[]
+
+    if array == []
+      return nil
+    else
+      #p = array[0]
+      for i in 0..array.length-1
+        #if array[i] > p
+          #p=array[i]
+        if p.empty?
+          p<<array[i]
+        elsif array[i] > p.last
+          p.push(array[i])
+        end
+      end
+    end
+    return p.last
+    #return p
   end
 
   def self.middle_element(array)
