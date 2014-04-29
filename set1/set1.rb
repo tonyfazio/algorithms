1cef06e342b315ad05badca611fe487ad664c99b
Tony Fazio <antoniojfazio@gmail.com>
2014-04-29 13:38:14 -0500

    swap complete

--- set1/set1.rb
+++ set1/set1.rb
@@ -1,5 +1,24 @@
 module Set1
   def self.swap_small(array)
+    if array.length == 1
+      return array
+    elsif array.length == 2
+      if array[0] > array[1]
+        array= [array[1],array[0]]
+      else
+        return array
+      end
+    else
+      for i in 0...array.length
+        if array[i]<array[0]
+          x = array[0]
+          y = array[i]
+          array[0] =y
+          array[i] = x
+        end
+      end
+      array
+    end
   end
 
   def self.find_sum_2(array, sum = 0)
